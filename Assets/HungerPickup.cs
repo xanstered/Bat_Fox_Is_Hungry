2011614832c13af6e8b1e1b58ac20af7f0e4a22d
xanstered <gzak2@edu.cdv.pl>
2024-04-06 22:14:04 +0200

    figa

---
+++ Assets/HungerPickup.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HungerPickup : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SliderMovement.Hunger += 50f;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
