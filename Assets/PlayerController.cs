6e0a14c085b66b8afe4a63eda9bee3271cb72ef8
xanstered <gzak2@edu.cdv.pl>
2024-04-06 17:33:59 +0200

    bat flip

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpAmount = 8f;
     public float speed;
     private float Move;
+    bool facingRight = true;
 
     // Update is called once per frame
     void Update()
@@ -20,5 +21,24 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(0, jumpAmount);
         }
+
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2 (moveInput * speed, rb.velocity.y);
+
+        if(moveInput < 0 && facingRight)
+        {
+            flip();
+        }
+
+        if(moveInput > 0 && !facingRight)
+        {
+            flip();
+        }
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
     }
 }
